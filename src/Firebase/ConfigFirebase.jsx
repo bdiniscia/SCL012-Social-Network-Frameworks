fad877cfeb34670fa552a4c130ecf46e29f06f6c
bdiniscia <bdiniscia@gmail.com>
2020-04-26 16:53:55 -0400

    Setting the enviorment

---
+++ src/Firebase/ConfigFirebase.jsx
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore'
+
+// Llave de firebase para poder acceder a la base de datos que tenemos en ella 
+const firebaseConfig = {
+  apiKey: "AIzaSyA9iPpEj17FzQzaUGcsNqSjc_SeWJ62ByM",
+  authDomain: "beer-me-up-app.firebaseapp.com",
+  databaseURL: "https://beer-me-up-app.firebaseio.com",
+  projectId: "beer-me-up-app",
+  storageBucket: "beer-me-up-app.appspot.com",
+  messagingSenderId: "183550811494",
+  appId: "1:183550811494:web:d3d34343810c5dd474fca7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore(); // Conectamos firebase con firestore
+export { db, firebase }
