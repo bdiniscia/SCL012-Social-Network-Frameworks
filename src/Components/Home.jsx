45cad2c557d680a0490a4d3ecbe8db4e45324e01
bdiniscia <bdiniscia@gmail.com>
2020-05-02 21:11:46 -0400

    Styles of the Navbar done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Components/Button'
 import { firebase, auth } from '../Firebase/ConfigFirebase'
+import Navbar from './Navbar'
 
 
 const Home = () => {
-
-    React.useEffect(() => {
-        
-    })
 
     // Función que cierra sesión
     const closeSession = () => {
@@ -23,6 +20,7 @@ const Home = () => {
 
     return (
         <div>
+            <Navbar />
             <h1>Home de la web</h1>
             <Button title='Cerrar Sesión' onClick={() => closeSession()}/>
 
