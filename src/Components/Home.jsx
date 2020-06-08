30d520fd23519277d6b3561f32d20fd937e7261b
bdiniscia <bdiniscia@gmail.com>
2020-06-08 00:41:20 -0400

    Upload and map real collection from Firebase

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from './Navbar'
 import CardPost from './Components/CardPost'
 import './Home.sass'
 import Modal from './Modal'
+import { db } from '../Firebase/ConfigFirebase'
 
 
 const Home = () => {
 
     const [showCreatePost, setShowCreatPost] = useState(false)
+    const [ beerPost, setBeerPost ] = useState(false)
+
+    useEffect(() => {
+        const postsUploaded = db.collection('posts').orderBy('time', 'desc');
+        postsUploaded.onSnapshot((querySnapshot) => {
+            const posts = [];
+    
+            querySnapshot.forEach(doc => {
+                const infoPost = 
+                  { dataBeer: doc.data(),
+                    id: doc.id }
+                posts.push(infoPost);
+            });
+
+            setBeerPost(posts)
+            console.log(posts)
+        });
+    }, [])
 
     const showModal = () => {
         setShowCreatPost(true)
@@ -23,38 +42,21 @@ const Home = () => {
             <Navbar showCreatePost={showCreatePost} showModal={() => showModal()}/>
             {showCreatePost && <Modal closeModal={() => closeModal()}/> }
             <section className='containerPosts'>
-                <CardPost 
-                    img={require('../img/img-post.jpg')} 
-                    profilePic={require('../img/user.png')} 
-                    author='Bdiniscia' 
-                    likes='12' 
-                    content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque gravida justo a pulvinar' 
-                    tags='#Artesanal'
-                />
-                <CardPost 
-                    img={require('../img/img-post.jpg')} 
-                    profilePic={require('../img/user.png')} 
-                    author='Bdiniscia' 
-                    likes='12' 
-                    content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque gravida justo a pulvinar' 
-                    tags='#Artesanal'
-                />
-                <CardPost 
-                    img={require('../img/img-post.jpg')} 
-                    profilePic={require('../img/user.png')} 
-                    author='Bdiniscia' 
-                    likes='12' 
-                    content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque gravida justo a pulvinar' 
-                    tags='#Artesanal'
-                />
-                <CardPost 
-                    img={require('../img/img-post.jpg')} 
-                    profilePic={require('../img/user.png')} 
-                    author='Bdiniscia' 
-                    likes='12' 
-                    content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque gravida justo a pulvinar' 
-                    tags='#Artesanal'
-                />
+                { beerPost &&
+                beerPost.map(post => {
+                    return (
+                        <CardPost 
+                            key = { post.id }
+                            img={post.dataBeer.img} 
+                            profilePic={post.dataBeer.user.photoURL} 
+                            author={post.dataBeer.user.displayName} 
+                            likes='12' 
+                            content={post.dataBeer.text}
+                            tags='#Artesanal'
+                        />
+                    )
+                })
+                }
             </section>
         </div>
     )
--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,17 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Modal.sass'
+import { db, storage } from '../Firebase/ConfigFirebase'
+import { useSelector, useDispatch } from 'react-redux'
 
-function Modal(props) {
+const Modal = (props) => {
+
+    const [textPost, setTextPost] = useState('')
+    const [imagePost, setImagePost] = useState('')
+    const currentUser = useSelector(state => state.currentUser)
+
+    const handleChangeText = (e) => {
+        e.preventDefault()
+        const text = e.target.value
+        setTextPost(text)
+    }
+
+    const handleChangeImage = (e) => {
+        e.preventDefault(e)
+        if (e.target.files[0]) {
+            console.log(e.target.files[0])
+            const image = e.target.files[0]
+            setImagePost(image)
+        }
+    } 
+
+    const handleUpload = () => {
+        return new Promise ((resolve, reject) => {
+            const image = imagePost
+            const uploadTask = storage.ref(`images/${image.name}`).put(image)
+            uploadTask.on('state_changed', 
+            (snapshot) => {
+                // progress function
+            }, (error) => {
+                // error function
+                console.log(error)
+                return reject(error)
+            }, () => {
+                // complete function
+                storage.ref('images').child(image.name).getDownloadURL().then(url => {
+                    return resolve(url)
+                })
+            })
+        })
+    }
+
+    const savePost = () => {
+        handleUpload()
+            .then(photo => {
+                db.collection('posts').add({
+                    user: currentUser,
+                    text: textPost,
+                    time: new Date(),
+                    img: photo,
+                })
+            })
+            .then(() => {
+                setTextPost('')
+                setImagePost('')
+                props.closeModal()
+            })
+            .catch((error) => {
+                console.log('Error ', error);
+            });
+    }
 
     return (
         <div className="modal">
             <div className="modal-content">
                 <span className="close" onClick={props.closeModal}>&times;</span>
-                <textarea className='textareaModal' placeholder='Cuéntanos... ¿Qué tal tu cerveza de hoy?'/>
-                <img alt='Save post' src={require('../img/send.png')} className='sendButton'/>
+                <textarea onChange={e => handleChangeText(e)} className='textareaModal' placeholder='Cuéntanos... ¿Qué tal tu cerveza de hoy?'/>
+                <input type='file' onChange={e => handleChangeImage(e)}/>
+                <img alt='Save post' src={require('../img/send.png')} onClick={() => savePost()} className='sendButton'/>
             </div>
         </div>
-
     )
 }
 
