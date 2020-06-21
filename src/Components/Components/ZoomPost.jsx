b10ab3ea7440355cc5ebb75390004b734e5f935d
Bárbara Di Niscia <bdiniscia@gmail.com>
2020-06-21 01:13:18 -0400

    Added the posts to the Redux Store. Implemented the like posts function (#6)

--- src/Components/Components/ZoomPost.jsx
+++ src/Components/Components/ZoomPost.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import './ZoomPost.sass'
+import React from 'react';
+import './ZoomPost.sass';
+import Heart from './Heart';
+import { postLike } from '../../Firebase/FirebaseFunctions';
+import { useSelector } from 'react-redux';
 
 const ZoomPost = (props) => {
+    const postsStore = useSelector(state => state.posts);
+    const index = props.post;
     
     return (
         <div className='overlayZoom'>
             <div className='divZoom'>
                 <span className="closeZoom" onClick={props.closeZoom}>&times;</span>
-                <img alt='Pic of the post' src={props.post.dataBeer.img} className='imgZoom' />
+                <img alt='Pic of the post' src={postsStore[index].dataBeer.img} className='imgZoom' />
                 <div className='contentPost contentZoom'>
                     <div className='idPost flexRow'>
                         <div className='authorDiv flexRow'>
-                            {props.post.dataBeer.user.photoURL ?
-                            <img alt='Profile pic of the author' src={props.post.dataBeer.user.photoURL} className='userPicPost' />
+                            {postsStore[index].dataBeer.user.photoURL ?
+                            <img alt='Profile pic of the author' src={postsStore[index].dataBeer.user.photoURL} className='userPicPost' />
                             :
                             <img alt='Profile pic of the author' src={require('../../img/user.png')} className='userPicPost' />
                             }
-                            <h5 className='authorPost margin0'>{props.post.dataBeer.user.displayName}</h5>
+                            <h5 className='authorPost margin0'>{postsStore[index].dataBeer.user.displayName}</h5>
                         </div>
                         <div className='likesDiv flexRow'>
-                            <h5 className='numLikes margin0'>12</h5>
-                            <img alt='Button of like' src={require('../../img/heart.png')} className='heartLike'/>
+                            <h5 className='numLikes margin0'>{postsStore[index].dataBeer.like.length}</h5>
+                            { postsStore[index].dataBeer.like.includes(postsStore[index].currentUser)
+                            ?
+                            <Heart className='heartLike likedPost' onClick={() => postLike(postsStore[index].id, postsStore[index].currentUser)}/>
+                            :
+                            <Heart className='heartLike' onClick={() => postLike(postsStore[index].id, postsStore[index].currentUser)}/>
+                            }
                         </div>
                     </div>
-                    <p className='textPost margin0'>{props.post.dataBeer.text}</p>
-                    {props.post.dataBeer.tag && <p className='tagsPost'>{props.post.dataBeer.tag}</p>}
+                    <p className='textPost margin0'>{postsStore[index].dataBeer.text}</p>
+                    {postsStore[index].dataBeer.tag && <p className='tagsPost'>{postsStore[index].dataBeer.tag}</p>}
                 </div>
             </div>
         </div>
