f03145721ae460c2a98cb7ded02798b415acb3d7
bdiniscia <bdiniscia@gmail.com>
2020-06-21 14:10:45 -0400

    Updates

---
+++ precache-manifest.30976e57bc95a189dd36028c1ddc91cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312cc41a27d98384039c9455dca38b47",
+    "url": "/SCL012-Social-Network-Frameworks/index.html"
+  },
+  {
+    "revision": "04ce21786ba9652f5498",
+    "url": "/SCL012-Social-Network-Frameworks/static/css/main.53fcdcc1.chunk.css"
+  },
+  {
+    "revision": "000b726cea86d678bc6d",
+    "url": "/SCL012-Social-Network-Frameworks/static/js/2.77259712.chunk.js"
+  },
+  {
+    "revision": "f0bdea3f8a058a1b4bc9663abf29e5a0",
+    "url": "/SCL012-Social-Network-Frameworks/static/js/2.77259712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ce21786ba9652f5498",
+    "url": "/SCL012-Social-Network-Frameworks/static/js/main.e1cfe567.chunk.js"
+  },
+  {
+    "revision": "f20e2b57517c4c332c3b",
+    "url": "/SCL012-Social-Network-Frameworks/static/js/runtime-main.6d0256c7.js"
+  },
+  {
+    "revision": "d5bba14d85ab135da8e8d4f7f29eb2c4",
+    "url": "/SCL012-Social-Network-Frameworks/static/media/back-1.d5bba14d.jpg"
+  },
+  {
+    "revision": "80204299aa1a3fa1f2e4f2171a673e87",
+    "url": "/SCL012-Social-Network-Frameworks/static/media/logoBeer.80204299.png"
+  },
+  {
+    "revision": "4f7f3e482450247f481fdfb4131f6a65",
+    "url": "/SCL012-Social-Network-Frameworks/static/media/logoBeerWhite.4f7f3e48.png"
+  }
+]);
